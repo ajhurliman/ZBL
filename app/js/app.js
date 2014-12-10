3daf196183a8504012fd0c93065848c81f38f57f
James Hurliman <james.hurliman@gmail.com>
2014-12-10 12:12:47 -0800

    flesh out html pages, add routing capability

--- app/js/app.js
+++ app/js/app.js
@@ -11,9 +11,9 @@ angular.module('zblApp', [
   // 'zblApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/landing-page-partial.html', controller: 'LandingPageCtrl'});
-  $routeProvider.when('/addQuery', {templateUrl: '/partials/add-query-partial.html', controller: 'AddQueryCtrl'});
-  $routeProvider.when('/manageQuery', {templateUrl: '/partials/manage-query-partial.html', controller: 'ManageQueryCtrl'});
-  $routeProvider.when('/manageAcct', {templateUrl: '/partials/manage-acct-partial.html', controller: 'ManageAcctCtrl'});
+  $routeProvider.when('/', {redirectTo: '/addQuery'});
+  $routeProvider.when('/addQuery', {templateUrl: '/partials/add-query-partial.html', controller: 'AddQueryController'});
+  $routeProvider.when('/manageQuery', {templateUrl: '/partials/manage-query-partial.html', controller: 'ManageQueryController'});
+  $routeProvider.when('/manageAcct', {templateUrl: '/partials/manage-acct-partial.html', controller: 'ManageAcctController'});
   $routeProvider.otherwise({redirectTo: '/addQuery'});
 }]);
---
+++ app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('zblApp.controllers', [])
+  .controller('AddQueryController', ['$scope', function($scope) {
+    $scope.testvariable = 'james';
+  }])
+  .controller('ManageQueryController', function() {
+
+  })
+  .controller('ManageAcctController', function() {
+
+  });
