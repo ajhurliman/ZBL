d08db72cdb14ce16fa79b1743a4db44c42cf0f81
James Hurliman <james.hurliman@gmail.com>
2014-12-10 23:08:02 -0800

    update tooling, re-wrote all the angular to 2.0 (was 1.5), deleteQuery() still broken

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,35 @@
 
 /* Controllers */
 
-angular.module('zblApp.controllers', [])
-  .controller('AddQueryController', ['$scope', function($scope) {
-    $scope.testvariable = 'james';
-  }])
-  .controller('ManageQueryController', function() {
+module.exports = function(controllers) {
+  controllers.controller('AddQueryController', ['$scope', '$firebase', function($scope, $firebase) {
+    var queryRef = new Firebase('https://zblapp.firebaseio.com');
+    $scope.queries = $firebase(queryRef);
 
-  })
-  .controller('ManageAcctController', function() {
+    $scope.newAsin = '';
+    $scope.saveProduct = function() {
+      //TODO: get categories from MWS API, then make a new query for each & add to firebase
+      //
+      var newQuery = {asin: $scope.newAsin, title: 'new title', paused: false};
+      $scope.queries.$push(newQuery);
+      $scope.newAsin = '';
+    };
+  }]);
 
+  controllers.controller('ManageQueryController', ['$scope', '$firebase', function($scope, $firebase) {
+    //3-way binding for list of queries (model + view + firebase)
+    var ref = new Firebase('https://zblapp.firebaseio.com');
+    var sync = $firebase(ref);
+    var syncObject = sync.$asObject();
+    syncObject.$bindTo($scope, 'queries');
+
+    $scope.deleteQuery = function(queryToDelete) {
+      syncObject.$remove(queryToDelete.$id);
+    };
+
+    }]);
+
+  controllers.controller('ManageAcctController', function() {
+    //manage acct contents
   });
+};
