6adac8ea6c19af603cfd5bd5744e7a3988756b21
James Hurliman <james.hurliman@gmail.com>
2015-01-22 16:54:41 -0800

    add protractor tests

--- test/client/conf.js
+++ test/client/conf.js
@@ -1,8 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
-  multiCapabilities: [
-  {browserName: 'firefox'},
-  {browserName: 'chrome'},
-  {browserName: 'safari'}]
+  specs: ['spec.js']
 };
--- test/client/spec.js
+++ test/client/spec.js
@@ -1,26 +1,59 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/zblDev');
+mongoose.connection.collections['querysets'].drop(function(err) {
+  if (err) throw err;
+  console.log('collection dropped');
+});
+
 describe('angularjs test', function() {
+  var apiUrlInput = element(by.model('newApiUrl'));
+  var newNameInput = element(by.model('newName'));
+  var addQueryBtn = element(by.id('addQueryBtn'));
+  var manageQueriesBtn = element(by.buttonText('Manage Queries'));
+  // var latestResult = element(by.binding('latest'));
+  // var history = element.all(by.repeater('result in memory'));
+
+  function addQuery(a, b) {
+    apiUrlInput.sendKeys('https://www.kimonolabs.com/api/asmpbme8?apikey=WsyckNRjMTAqCPdYCkxImCwX2jSN0sk8&kimseries=1');
+    newNameInput.sendKeys('sample api');
+    addQueryBtn.click();
+  }
+
   beforeEach(function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+    browser.get('http://www.sandbox.com');
   });
 
   it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('Super Calculator');
+    expect(browser.getTitle()).toEqual('Zapp Bug Logger');
   });
 
-  it('should add one and two', function() {
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  it('should add a query', function() {
 
-    element(by.id('gobutton')).click();
-
-    expect(element(by.binding('latest')).getText()).toEqual('3');
   });
 
-  it('should add 4 and 6', function() {
-    element(by.model('first')).sendKeys(4);
-    element(by.model('second')).sendKeys(6);
-    element(by.id('gobutton')).click();
+  // it('should have a history', function() {
+  //   add(1, 2);
+  //   add(3, 4);
 
-    expect(element(by.binding('latest')).getText()).toEqual('10');
-  });
+  //   expect(history.count()).toEqual(2);
+
+  //   add(5, 6);
+
+  //   expect(history.count()).toEqual(3);
+  // });
+
+
+
+  // it('should add one and two', function() {
+  //   add(1, 2);
+
+  //   expect(latestResult.getText()).toEqual('3');
+  // });
+
+  // it('should add 4 and 6', function() {
+  //   add(4, 6);
+
+  //   expect(latestResult.getText()).toEqual('10');
+  // });
 });
